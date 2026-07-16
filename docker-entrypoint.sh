95d177027fdae97198e54a0cbead0c78a80f9ac1
EriNunu <hntdpps423@gmail.com>
2026-07-16 16:30:35 +0900

    add: add docker-entrypoint.sh for Docker

---
+++ docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Renderが割り当てるPORT環境変数に合わせてApacheの設定を書き換える
+sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
+sed -i "s/:80>/:${PORT:-80}>/g" /etc/apache2/sites-available/000-default.conf
+
+exec "$@"
